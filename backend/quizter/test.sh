1ee09c1b7c2057ce55a85a8bf1c17a9dee9c7a31
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 13:46:27 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -1,6 +1,4 @@
 c#!/bin/bash
 
 echo "Running python manage.py testing command:"
-pip3 install -r requirement.txt
-pip3 install dj-database-url
-python3 manage.py test quizApp/tests 
+python3 manage.py runserver
