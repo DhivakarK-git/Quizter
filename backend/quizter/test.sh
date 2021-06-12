35da429165ae067789302516d59a67819af7bcb4
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 17:07:56 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -2,7 +2,7 @@ c#!/bin/bash
 
 echo "Running python manage.py testing command:"
 wget http://www.sqlite.org/2016/sqlite-autoconf-3150000.tar.gz
-tar xvfz sqlite-autoconf-3071502.tar.gz
-cd sqlite-autoconf-3071502
+tar xvfz sqlite-autoconf-3150000.tar.gz
+cd sqlite-autoconf-3150000
 ./configure --prefix=/usr/local
 make install
