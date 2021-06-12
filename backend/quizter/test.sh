5c9a5550049dc08cdcd9a72c4539438e06800278
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 16:51:28 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -1,5 +1,5 @@
 c#!/bin/bash
 
 echo "Running python manage.py testing command:"
-pip3.9 install dj-database-url
+pip3.9 install django-cors-headers django-graphql-auth graphene-django pillow whitenoise django-graphene-permissions
 python3.9 manage.py test quizApp/tests
