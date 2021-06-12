8fa3349d25091b5a3c03da03c4031c78cf0873e4
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 18:05:08 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -2,6 +2,6 @@ c#!/bin/bash
 
 echo "Running python manage.py testing command:"
 export LD_LIBRARY_PATH=/usr/local/lib
-pip3.9 install sqlite3
+pip3.9 install pysqlite3 
 python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
 sqlite3 --version 
