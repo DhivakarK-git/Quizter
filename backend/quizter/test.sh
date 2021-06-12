65ba748541bcdc330064ea2b90895890a87490a5
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 17:22:02 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -6,6 +6,5 @@ tar zxvf sqlite-autoconf-3240000.tar.gz
 ./configure --prefix=/usr/local
 make
 sudo make install
-python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
 export LD_LIBRARY_PATH=/usr/local/lib
-python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
+# python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
