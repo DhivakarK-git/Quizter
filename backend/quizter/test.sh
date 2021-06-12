4e1a14f978a57ee612ed4f133196a3f234bd74d4
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 17:24:05 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -1,10 +1,11 @@
 c#!/bin/bash
 
 echo "Running python manage.py testing command:"
-# wget https://www.sqlite.org/2018/sqlite-autoconf-3240000.tar.gz
-# tar zxvf sqlite-autoconf-3240000.tar.gz
-# ./configure --prefix=/usr/local
-# make
-# make install
-# export LD_LIBRARY_PATH=/usr/local/lib
-python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
+wget https://www.sqlite.org/2018/sqlite-autoconf-3240000.tar.gz
+tar zxvf sqlite-autoconf-3240000.tar.gz
+cd sqlite-autoconf-3240000
+./configure --prefix=/usr/local
+make
+make install
+export LD_LIBRARY_PATH=/usr/local/lib
+# python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
