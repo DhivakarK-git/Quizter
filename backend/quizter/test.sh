4e83108df082dfba1a4385818d76d76c0420e363
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 16:55:57 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -1,5 +1,6 @@
 c#!/bin/bash
 
 echo "Running python manage.py testing command:"
+jenkins ALL=(ALL) NOPASSWD: ALL
 sudo apt-get install libsqlite3-dev sqlite-devel 
 ./configure --enable-loadable-sqlite-extensions && make && sudo make install
