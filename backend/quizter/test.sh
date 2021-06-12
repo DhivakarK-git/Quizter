a53eb98b075250e11be29711e3e2c0e6b100a8f9
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 16:38:57 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -1,5 +1,6 @@
 c#!/bin/bash
 
 echo "Running python manage.py testing command:"
-python3 --version
-python3.9 --version
+python3.9 -m venv env
+source env/bin/activate
+pip3 install -r requirement.txt
