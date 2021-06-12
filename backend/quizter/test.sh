b9f0ae98a4e36f40620b13ba9bf73bc9d722a8e3
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 16:43:41 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -2,7 +2,5 @@ c#!/bin/bash
 
 echo "Running python manage.py testing command:"
 source env/bin/activate
-sudo apt-get install python3.9-pip
-pip3.9 -v
-# pip3 install django
-# python3.9 manage.py test quizApp/tests
+pip3.9 install -r requirement.txt
+python3.9 
