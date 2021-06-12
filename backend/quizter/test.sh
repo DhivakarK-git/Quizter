421d7c566abfc94b7b4eceee16c2fee6db1758b7
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 17:27:51 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -8,4 +8,5 @@ echo "Running python manage.py testing command:"
 # make
 # make install
 # export LD_LIBRARY_PATH=/usr/local/lib
-python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
+# python3.9 -c "import sqlite3; print(sqlite3.sqlite_version)"
+sqlite3 --version 
