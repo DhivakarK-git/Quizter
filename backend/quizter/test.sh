532c4210804a0072195d4e8d0dad47d3033b1af7
Dhivakar K <78305631+DhivakarK-git@users.noreply.github.com>
2021-06-12 16:54:33 +0530

    Update test.sh

--- backend/quizter/test.sh
+++ backend/quizter/test.sh
@@ -3,4 +3,3 @@ c#!/bin/bash
 echo "Running python manage.py testing command:"
 sudo apt-get install libsqlite3-dev sqlite-devel 
 ./configure --enable-loadable-sqlite-extensions && make && sudo make install
-python3.9 manage.py test quizApp/tests
