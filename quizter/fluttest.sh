140e7943a1babe7a290d1a18f7a16f6a2ad25d8d
ramyaachanta <64644325+ramyaachanta@users.noreply.github.com>
2021-06-14 22:51:15 +0530

    Update fluttest.sh

--- quizter/fluttest.sh
+++ quizter/fluttest.sh
@@ -1,4 +1,8 @@
 c#!/bin/bash
 
-echo "Running flutter testing command:"
-flutter test test/unit_test.dart
+# echo "Running flutter unit testing command:"
+# flutter test test/unit_test.dart
+
+
+echo "Running flutter widget testing command:"
+flutter test test/widget_test.dart
