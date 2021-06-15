34592e884ef208533678143ae423cc3a9cc11269
ramyaachanta <64644325+ramyaachanta@users.noreply.github.com>
2021-06-15 16:19:41 +0530

    Update test1.sh

--- backend/test1.sh
+++ backend/test1.sh
@@ -1,7 +1,7 @@
 c#!/bin/bash
 usermod -aG docker jenkins
 usermod -aG root jenkins
-chmod 664 /var/run/docker.sock
+chmod +x /var/run/docker.sock
 mkdir -p .pip_cache
 docker build -t djangodemo .
 docker run -it -p 8020:8020 \
